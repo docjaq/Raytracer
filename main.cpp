fe273670d33f0ea7fbe5f722c27c927a50292c46
docjaq <jonaquinn@gmail.com>
2020-12-10 11:40:05 +0000

    Camera model and aa

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include "utility.h"
+#include "camera.h"
 #include "hittable_list.h"
 #include "color.h"
 #include "sphere.h"
@@ -23,20 +24,14 @@ int main() {
     std::ofstream outputPPM;
     outputPPM.open ("Output.ppm");
 
-	//Frame
-	const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1024;
-    const int image_height = static_cast<int>(image_width/aspect_ratio);
-    outputPPM << "P3\n" << image_width << " " << image_height << "\n255\n";
+	auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 1000;
 
-	//Camera
-	auto viewport_height = 2.0;
-	auto viewpoirt_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewpoirt_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	outputPPM << "P3\n" << image_width << " " << image_height << "\n255\n";
+	
+	camera cam;
 
 	//World
 	auto ground = make_shared<sphere>(point3(0, -100.5, -1), 100);
@@ -50,15 +45,20 @@ int main() {
 
         for(int i = 0; i < image_width; ++i){
 
-            //Send ray from origin, through offset position
-            double u = double(i)/double(image_width-1);
-            double v = double(j)/double(image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+			color pixel_color(0, 0, 0);
 
-            //vec3 p = r.point_at_parameter(2.0f);
-           
-			color pixel_color = ray_color(r, world);
-			write_color(outputPPM, pixel_color);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+
+				//Send ray from origin, through offset position
+				double u = (i+random_double()) / (image_width - 1);
+				double v = (j+random_double()) / (image_height - 1);
+
+				ray r = cam.get_ray(u, v);
+
+				pixel_color += ray_color(r, world);
+
+			}
+			write_color(outputPPM, pixel_color, samples_per_pixel);
         }
     }
 
--- utility.h
+++ utility.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -14,6 +15,20 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937(generator);
+	return distribution(generator);
+}
+
+inline double clamp(double value, double min, double max) {
+
+	if (value < min) return min;
+	if (value > max) return max;
+
+	return value;
 }
 
 #include "ray.h"
