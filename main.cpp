e2e38d4f547eefce305fe9bb54e07f73eef960c5
docjaq <jonaquinn@gmail.com>
2020-12-11 17:24:34 +0000

    Final render setup.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,56 @@
 #include "sphere.h"
 #include "material.h"
 
+//Test scene render
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
+}
+
+
 color ray_color(const ray& r, hittable& world, int depth){
 
     hit_record rec;
@@ -37,43 +87,26 @@ int main() {
     std::ofstream outputPPM;
     outputPPM.open ("Output.ppm");
 
-	auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	//Image
+	auto aspect_ratio = 3.0 / 2.0;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
+	const int samples_per_pixel = 500;
 	const int max_depth = 50;
 
 	outputPPM << "P3\n" << image_width << " " << image_height << "\n255\n";
 	
-	point3 cameraPosition(3, 3, 2);
-	point3 lookAtPosition(0, 0, -1);
+	//Camera
+	point3 cameraPosition(13, 2, 3);
+	point3 lookAtPosition(0, 0, 0);
 	vec3 up(0, 1, 0);
 	double fov = 20;
-	double aperture = 0.5;
-	double dist_to_focus = (cameraPosition - lookAtPosition).length();
-
+	double aperture = 0.1;
+	double dist_to_focus = 10.0;
 	camera cam(cameraPosition, lookAtPosition, up, fov, aspect_ratio, aperture, dist_to_focus );
 	
-	//Materials
-	auto shirley_material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto shirley_material_centre = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto shirley_material_left = make_shared<dielectric>(1.5);
-	auto shirley_material_right = make_shared<metal>(color(1.0, 0.6, 0.6), 0.0);
-
 	//World
-	hittable_list world;
-	auto ground = make_shared<sphere>(point3(0, -100.5, -1), 100, shirley_material_ground);
-	
-	auto shirley_left_inner = make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, shirley_material_left);
-	auto shirley_left_outer = make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, shirley_material_left);
-	auto shirley_right = make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, shirley_material_right);
-	auto shirley_centre = make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, shirley_material_centre);
-
-	world.add(std::move(ground));
-	world.add(std::move(shirley_left_inner));
-	world.add(std::move(shirley_left_outer));
-	world.add(std::move(shirley_right));
-	world.add(std::move(shirley_centre));
+	auto world = random_scene();
 
     for(int j = image_height-1; j >= 0; --j){
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
