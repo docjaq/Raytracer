593519c5dc8efb8069264c611f6a10eb7acf613e
docjaq <jonaquinn@gmail.com>
2020-12-16 10:08:54 +0000

    Small tidy up and modifications to final scene.

--- main.cpp
+++ main.cpp
@@ -17,34 +17,37 @@ std::mutex mu;
 hittable_list random_scene() {
 	hittable_list world;
 
-	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	auto ground_material = make_shared<metal>(color(0.4, 0.4, 0.45), .3);
 	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
 	for (int a = -11; a < 11; a++) {
 		for (int b = -11; b < 11; b++) {
 			auto choose_mat = random_double();
-			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+			double size = random_double(0.1, 0.2);
+			point3 center(a + 0.9*random_double(), size-0.02, b + 0.9*random_double());
 
 			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
 				shared_ptr<material> sphere_material;
+
+				
 
 				if (choose_mat < 0.8) {
 					// diffuse
 					auto albedo = color::random() * color::random();
 					sphere_material = make_shared<lambertian>(albedo);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+					world.add(make_shared<sphere>(center, size, sphere_material));
 				}
 				else if (choose_mat < 0.95) {
 					// metal
 					auto albedo = color::random(0.5, 1);
 					auto fuzz = random_double(0, 0.5);
 					sphere_material = make_shared<metal>(albedo, fuzz);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+					world.add(make_shared<sphere>(center, size, sphere_material));
 				}
 				else {
 					// glass
 					sphere_material = make_shared<dielectric>(1.5);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+					world.add(make_shared<sphere>(center, size, sphere_material));
 				}
 			}
 		}
@@ -87,7 +90,7 @@ color ray_color(const ray& r, hittable& world, int depth){
     vec3 unit_direction = unit_vector(r.direction());
     double t = 0.5*(unit_direction.y() + 1.0);
     
-	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.6, 0.6, 0.7);
 }
 
 //Iterative version instead...
@@ -129,10 +132,10 @@ void LockedOutput(int totalScanLines, int finishedScanLines){
 int main() {
 
 	//Image
-	auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 1200;
+	auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 1920;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 20;
+	const int samples_per_pixel = 500;
 	const int max_depth = 50;
 
 	//Camera
